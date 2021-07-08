1b2e4de37c2e159f61840b4c37a9eab8737f5218
Allen Pottinger <allenpottinger@gmail.com>
2021-07-07 23:04:41 -0400

    adds header js click event

---
+++ index.js
@@ -0,0 +1,10 @@
+let firstHeader = document.getElementById('meet')
+
+firstHeader.addEventListener('click', ()=> {
+    let dis = firstHeader.innerHTML += `
+    <div>
+    <h3>Tyler & Kelli are a creative couple who have a love of faith, family & art!</h3>
+    </div>
+    `
+})
+
